7b6830bf7918625a76976953a29c5aa8f6804da3
hakehardware <alex@alexhake.com>
2024-10-05 12:40:43 -0700

    fixed edit URLs

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -40,7 +40,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/hakehardware/hakehardware.github.io/edit/deployment/',
+            'https://github.com/hakehardware/hakehardware.github.io/edit/main/',
         },
         blog: {
           showReadingTime: true,
@@ -51,7 +51,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/hakehardware/hakehardware.github.io/edit/deployment/',
+            'https://github.com/hakehardware/hakehardware.github.io/edit/main/',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
